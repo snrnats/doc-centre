cfb5c91006d29a723dc492e394cff305e850ca01
Michael Rose <michael.rose@sorted.com>
2020-03-25 10:32:54 +0000

    Don't throw error on missing environment variable

--- dockerbuild.ps1
+++ dockerbuild.ps1
@@ -1,7 +1,7 @@
 $authToken = $env:GITHUB_AUTH_TOKEN;
 
 if ([string]::IsNullOrWhiteSpace($authToken)) {
-    throw "The environment variable GITHUB_AUTH_TOKEN is missing. This should be {username}:{token} encoded in base64."
+    Write-Host "Warning :: the environment variable GITHUB_AUTH_TOKEN is missing. This should be {username}:{token} encoded in base64. Commit/author data will be omitted"
 }
 
 Write-Host "Initiating Docker build step"
