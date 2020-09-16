00664636aabce9e4e30ffea942c18732ae285271
andywalton7 <47785489+andywalton7@users.noreply.github.com>
2020-09-16 17:01:51 +0100

    Started new consignments API spec

---
+++ docfx_project/pro/scripts/dropdown.js
@@ -0,0 +1,11 @@
+function dropFunction(a) {
+  a.parentElement.getElementsByClassName('dropdown-content')[0].classList.toggle("show");
+}
+
+/*function dropFunction(id) {
+  document.getElementById(id).classList.toggle("show");
+}*/
+
+/*function dropFunction(a, b) {
+  a.parentElement.getElementsByClassName(b)[0].classList.toggle("show");
+}*/
